b2795eaf74a414bfdd00096fcb2e96524aa8ce3e
Anatolie Lupacescu <anatollupacescu@gmail.com>
2018-10-22 18:55:19 +0200

    little on containers

--- containers/containers_test.go
+++ containers/containers_test.go
@@ -1,19 +1,61 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+	"unsafe"
+)
 
 type (
-  valueMap map[int]string
-  pointerMap  map[int]*string
-)
-func processMap(data valueMap) {
 
+	userName [128]byte
+
+	user struct {
+		name userName
+	}
+
+	valueMap map[int]user
+
+	pointerMap map[int]*user
+)
+
+func processValueMap(data valueMap) {
+	fmt.Printf("size of value map: %v bytes\n", unsafe.Sizeof(data))
+	data[0] = newDummyUser("value")
 }
 
 func processPointerMap(data pointerMap) {
-
+	fmt.Printf("size of pointer map: %v bytes\n", unsafe.Sizeof(data))
+	s := newDummyUser("pointer")
+	data[0] = &s
 }
 
 func Test(t *testing.T) {
-  var v1
+	s := "initial"
+
+	u := newDummyUser(s)
+
+	fmt.Printf("size of the user: %v\n", unsafe.Sizeof(u))
+
+	valueMap := map[int]user{0: u}
+
+	pointerMap := map[int]*user{0: &u}
+
+	processValueMap(valueMap)
+	assertNotEqual(valueMap[0], u, t)
+
+	processPointerMap(pointerMap)
+	assertNotEqual(*pointerMap[0], u, t)
+}
+
+func assertNotEqual(u1 user, u2 user, t *testing.T) {
+	if u1 == u2 {
+		t.Fatal("Users are equal")
+	}
+}
+
+func newDummyUser(s string) user {
+	var name userName
+	copy(name[:], s)
+	return user{name: name}
 }
